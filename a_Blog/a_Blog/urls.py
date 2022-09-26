c34a3460ddd3ca8fe1330203d77988522f94bafb
AlexFromSiberia <id764g@gmail.com>
2022-09-26 16:34:09 +0700

    Настройка админки

--- a_Blog/a_Blog/urls.py
+++ a_Blog/a_Blog/urls.py
@@ -22,9 +22,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
 if settings.DEBUG:
+    import debug_toolbar
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
--- a_Blog/main/admin.py
+++ a_Blog/main/admin.py
@@ -1,9 +1,41 @@
 from django.contrib import admin
 from .models import *
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django import forms
+from django.utils.safestring import mark_safe
+
+
+# form for ckEditor
+class PostAdminForm(forms.ModelForm):
+    # content - field in our model
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Post
+        fields = '__all__'
 
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title', )}
+    form = PostAdminForm
+    save_as = True
+    # from your model (то что будет в СПИСКЕ всех записей)
+    list_display = ('id', 'title', 'slug', 'author', 'created_at', 'photo', 'get_photo', 'views', 'category')
+    # поля также можно сделать ссылками
+    list_display_links = ('title', 'category', )
+    # добавить поля по которым можно делать поиск
+    search_fields = ('author', )
+    # по каким полям будет возможность фильтра
+    list_filter = ('author', 'category', 'tags')
+    readonly_fields = ('id', 'created_at', 'get_photo')
+    def get_photo(self, obj):
+        # избежать ошибки если фото не существует
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="75">')
+        else:
+            return '-'
+
+    get_photo.short_description = 'Миниатюра'
 
 
 class TagAdmin(admin.ModelAdmin):
