1a8772c586dd7c4cb5b6a07ec9f06b7b872983d2
AlexFromSiberia <id764g@gmail.com>
2022-10-06 09:16:23 +0700

    Added: - sidebar tags

---
+++ a_Blog/main/templatetags/sidebar.py
@@ -0,0 +1,17 @@
+from django import template
+from main.models import Post, Tag
+
+register = template.Library()
+
+
+@register.inclusion_tag('main/popular_posts_tpl.html')
+def get_popular_post(cnt=3):
+    posts = Post.objects.order_by('-views')[:cnt]
+    return {'posts': posts}
+
+
+@register.inclusion_tag('main/tags_tpl.html')
+def get_tags_post():
+    tags = Tag.objects.all
+    return {'tags': tags}
+
--- a_Blog/main/views.py
+++ a_Blog/main/views.py
@@ -48,7 +48,7 @@ class GetPost(DetailView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        self.object.views =F('views') + 1
+        self.object.views = F('views') + 1
         self.object.save()
         self.object.refresh_from_db()
         return context
